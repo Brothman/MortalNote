b56ed4280df55c4a96ae8908e21a91a9432b1107
Benji Rothman <brothman7000@gmail.com>
2018-09-14 10:06:51 -0400

    Added and styled the whymortalnote landing page

--- frontend/components/component_wrapper.jsx
+++ frontend/components/component_wrapper.jsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import LoginForm from './login_form/login_form_container.js';
 import SignUpForm from './signup_form/signup_form_container.js';
 import LandingPage from './landing_page/landing_page_container.jsx';
+import WhyMortalNote from './why_mortalnote/why_mortalnote_container.jsx';
 
 import { AuthRoute } from '../utils/route_util.jsx';
 import { ProtectedRoute } from '../utils/route_util.jsx';
@@ -11,6 +12,7 @@ const ComponentWrapper = (props) => {
   return (
     <div>
       <Route exact path='/' component={LandingPage} />
+      <Route path="/why-mortalnote" component={WhyMortalNote} />
       <AuthRoute path='/login' component={LoginForm} />
       <AuthRoute path='/signup' component={SignUpForm} />
     </div>
---
+++ frontend/components/why_mortalnote/why_mortalnote_container.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from '../landing_page/header.jsx';
+
+class WhyMortalNote extends React.Component {
+  render () {
+    return (
+      <div className="why-mortalnote-grid">
+        <Header />
+        <h1 className="why-mortalnote-htag">Why MortalNote?</h1>
+        <p className="why-mortalnote-hidden-potential">
+          {"MortalNote helps people realize their hidden potential. It's where "}
+          dreams become reality, where life dances with death, and where
+          humans become gods.
+        </p>
+        <p className="why-mortalnote-evernote-better">
+          {"See why virtually 0 users implement MoralNote's products. "}
+          {"Hint: it's because Evernote is better."}
+        </p>
+        <img className="why-mortalnote-img" src="https://s3.us-east-2.amazonaws.com/mortalnote-images/why-evernote-hero-desktop.png"/>
+      </div>
+    );
+  }
+}
+
+export default WhyMortalNote;
