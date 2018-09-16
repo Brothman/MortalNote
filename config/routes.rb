953df59efee08fc80184678fe9b1afa4a5edf752
Ben Rothman <benji@Bens-MacBook-Pro.local>
2018-09-16 19:48:30 -0400

    A lot happened today. We setup the notebooks page to bare functionality. You see all of the current users notebooks, anda dropdown menu displays all the notes in that notebook. It remains to be implemented to close the dropdown. The sidebar is not fully styled. Auth remains to be completed with error messages tomorrow morning. Good work today Benji

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#root"
-  get 'auth/:provider/callback', to: 'api/sessions#google_create', defaults: { format: :json }
-  # get "*path", to: "static_pages#root"
 
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:show, :create]
@@ -14,4 +12,7 @@ Rails.application.routes.draw do
     resources :notebooks, only: [:create, :destroy, :show, :index]
     resources :notes, only: [:create, :destroy, :show]
   end
+  
+  get 'auth/:provider/callback', to: 'api/sessions#google_create', defaults: { format: :json }
+  get "*path", to: "static_pages#root"
 end
