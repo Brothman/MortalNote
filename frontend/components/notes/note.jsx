7158212208c56c6dd3ee250055ec440124289c5c
Benji Rothman <brothman7000@gmail.com>
2018-09-18 16:11:43 -0400

    Styled the notes page, specifically the notes index items

--- frontend/components/notes/note.jsx
+++ frontend/components/notes/note.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
 
-const Note = (props) => {
-  return (
-    <div className="note"/>
-  )
+class Note extends React.Component {
+  componentDidMount() {
+    // const toolbarOptions = [
+    //   ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    //   ['blockquote', 'code-block'],
+    //
+    //   [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    //   [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    //   [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    //   [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    //   [{ 'direction': 'rtl' }],                         // text direction
+    //
+    //   [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    //   [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    //
+    //   [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    //   [{ 'font': [] }],
+    //   [{ 'align': [] }],
+    //
+    //   ['clean']                                         // remove formatting button
+    // ];
+
+    const editor = new Quill('#editor', {
+      modules: { toolbar: '#toolbar' },
+      theme: 'snow'
+    });
+  }
+
+  render () {
+
+    return (
+      <div className="note">
+
+        <div id="toolbar">
+
+           <select className="ql-size">
+             <option value="small"></option>
+             <option defaultValue></option>
+             <option value="large"></option>
+             <option value="huge"></option>
+           </select>
+
+           <button className="ql-bold"></button>
+
+           <button className="ql-script" value="sub"></button>
+           <button className="ql-script" value="super"></button>
+        </div>
+
+        <div id="editor">
+          <p className="note-text">Hello World!</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Note;
--- frontend/components/notes/note_index.jsx
+++ frontend/components/notes/note_index.jsx
@@ -15,7 +15,7 @@ const NoteIndex = ( { notes } ) => {
       <h6 className="note-index-header"> All Notes </h6>
 
       <div className="note-index-top-items">
-        <span className="number-of-notes"> 1921 Notes </span>
+        <span className="number-of-notes"> {notes.length} Notes </span>
         <img className="note-index-action-icon" src="https://s3.us-east-2.amazonaws.com/mortalnote-images/evernote-svgs/actions-big-icon.svg" />
         <img className="note-index-tag-icon" />
       </div>
