cd8a59b4e040fb5bc7843d47a3c373f9306dd8c7
Benji Rothman <brothman7000@gmail.com>
2018-09-21 13:38:34 -0400

    Changed default loggedin page to /notes instead of /notebooks

--- frontend/utils/route_util.jsx
+++ frontend/utils/route_util.jsx
@@ -9,7 +9,7 @@ const Auth = ( {component: Component, path, loggedIn, exact} ) => (
     !loggedIn? (
       <Component {...props} />
     ): (
-      <Redirect to="/notebooks" />
+      <Redirect to="/notes" />
     )
   )} />
 );
