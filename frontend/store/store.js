96433343d0f755e73a78f8a10547d8082732a0a3
Benji Rothman <brothman7000@gmail.com>
2018-10-10 21:26:37 -0400

    commented out logger for heroku

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -7,10 +7,11 @@ import rootReducer from '../reducers/root_reducer.js';
 // basic middleware package
 const middlewares = [thunk];
 //add redux logger if we're not in production
-if (process.env.NODE_ENV !== "production") {
-  const { logger } = require('redux-logger');
-  middlewares.push(logger);
-}
+
+// if (process.env.NODE_ENV !== "production") {
+//   const { logger } = require('redux-logger');
+//   middlewares.push(logger);
+// }
 
 const configureStore = (preLoadedState = {}) => {
   return createStore(rootReducer,
